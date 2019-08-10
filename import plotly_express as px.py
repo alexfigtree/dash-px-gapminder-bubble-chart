7bfbd03af1297aef113c55414937f4ba92685684
Alex <alexandra.higuera@gmail.com>
2019-08-10 18:04:00 -0400

    bubble chart with gapminder data - no dash

---
+++ import plotly_express as px.py
@@ -0,0 +1,8 @@
+import plotly_express as px
+
+gapminder = px.data.gapminder()
+
+fig = px.scatter(gapminder.query("year==2007"), x="gdpPercap", y="lifeExp",
+             size="pop", color="continent",
+                 hover_name="country", log_x=True, size_max=60)
+fig.show()
