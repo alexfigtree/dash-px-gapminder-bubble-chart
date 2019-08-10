facb8636129264a86df94184296650bb493856f3
Alex <alexandra.higuera@gmail.com>
2019-08-10 18:48:10 -0400

    Incorporate dash into bubble chart

--- app.py
+++ app.py
@@ -1,8 +1,34 @@
 import plotly_express as px
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
 
-gapminder = px.data.gapminder()
+app = dash.Dash(
+    __name__, external_stylesheets=["https://codepen.io/chriddyp/pen/bWLwgP.css"]
+)
 
-fig = px.scatter(gapminder.query("year==2007"), x="gdpPercap", y="lifeExp",
-             size="pop", color="continent",
-                 hover_name="country", log_x=True, size_max=60)
-fig.show()
+def generate_bubble_chart():
+    return px.scatter(
+        px.data.gapminder(), 
+        x="gdpPercap", 
+        y="lifeExp", 
+        animation_frame="year", 
+        animation_group="country",
+        size="pop", 
+        color="country", 
+        hover_name="country", 
+        log_x = True, 
+        size_max=45, 
+        range_x=[100,100000], 
+        range_y=[25,90]
+    )
+
+app.layout = html.Div([
+    dcc.Graph(
+        id='scatter',
+        figure=generate_bubble_chart()
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
