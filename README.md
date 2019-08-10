a5e42cd7bbab77c67efd82a100a444dc84a47487
Alex <alexandra.higuera@gmail.com>
2019-08-10 19:44:27 -0400

    Tests from Dash / Plotly documentation; update readme

--- README.md
+++ README.md
@@ -2,13 +2,19 @@
 Income vs Life Expectancy by Country from 1952 to 2007
 
 ## To get started:
-- pip3 install plotly_express
-- pip3 install dash
+```
+pip3 install plotly_express
+pip3 install dash
+```
+
 
 
 ## To start the app: 
-- python3 app.py 
-- On Google Chrome, navigate to http://127.0.0.1:8050/ or whatever port specified in your terminal
+```
+python3 app.py 
+```
+
+On Google Chrome, navigate to http://127.0.0.1:8050/ or whatever port specified in your terminal
 
 ### Features: 
 - Deselect/select specific countries by either clicking on their respective bubbles or on the list on the right.
@@ -16,6 +22,16 @@ Income vs Life Expectancy by Country from 1952 to 2007
 
 
 ## Testing
-- pip3 install dash[testing]
-- install web drivers for Google Chrome (see https://dash.plot.ly/testing)
-- pytest tests.py
+```
+pip3 install dash[testing]
+```
+
+Install web drivers for Google Chrome (see https://dash.plot.ly/testing), then: 
+
+```
+pytest tests.py
+```
+
+## To Do: 
+- write more tests for dash_core_components
+- convert small colored circles on right hand side list to checkboxes
--- tests.py
+++ tests.py
@@ -1,11 +1,30 @@
-import time
-from selenium import webdriver
+# 1. imports of your dash app
+import dash
+import dash_html_components as html
 
-driver = webdriver.Chrome('chromedriver')  # Optional argument, if not specified will search path.
-driver.get('http://www.google.com/xhtml');
-time.sleep(5) # Let the user actually see something!
-search_box = driver.find_element_by_name('q')
-search_box.send_keys('ChromeDriver')
-search_box.submit()
-time.sleep(5) # Let the user actually see something!
-driver.quit()
+
+# 2. give each testcase a tcid, and pass the fixture
+# as a function argument, less boilerplate
+def test_bsly001_falsy_child(dash_duo):
+
+    # 3. define your app inside the test function
+    app = dash.Dash(__name__)
+    app.layout = html.Div(id="nully-wrapper", children=0)
+
+    # 4. host the app locally in a thread, all dash server configs could be
+    # passed after the first app argument
+    dash_duo.start_server(app)
+
+    # 5. use wait_for_* if your target element is the result of a callback,
+    # keep in mind even the initial rendering can trigger callbacks
+    dash_duo.wait_for_text_to_equal("#nully-wrapper", "0", timeout=4)
+
+    # 6. use this form if its present is expected at the action point
+    assert dash_duo.find_element("#nully-wrapper").text == "0"
+
+    # 7. to make the checkpoint more readable, you can describe the
+    # acceptance criterion as an assert message after the comma.
+    assert dash_duo.get_logs() == [], "browser console should contain no error"
+
+    # 8. visual testing with percy snapshot
+    dash_duo.percy_snapshot("bsly001-layout")
